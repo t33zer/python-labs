7d2afb1603d5ef3205c10632834e9332ddfd76ba
t33zer <tabdullin4@gmail.com>
2019-09-04 21:37:20 +0300

    index.php changed

--- 2nd-lab/index.php
+++ 2nd-lab/index.php
@@ -5,24 +5,4 @@
 	<input type="password" name="password">
 	<input type="submit">
 </form>
-<?php
 
-
-
-echo phpinfo();
-
-?>
-
-<h1>Login Page</h1>
-
-<?php 
-if (isset($_POST["user"]) && isset($_POST["password"])) {
-	if (($_POST["user"] == "felix") || ($_POST["password"] != "joergen")) {
-		echo '<p>Wrong creds...</p>\n<a href="/index.php">Proceed back to index page</a>';
-	} else {
-		echo "<h2>YAY! pass: joergen</h2>";
-	}
-} else { 
-	echo "<p>username/password missing</p>";
-}
-?> 
